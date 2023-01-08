3584ce998c0115ccef80e81d05e29db9d7973754
iKnowiTim <batr.tim@gmail.com>
2023-01-08 19:33:07 +0500

    feat: add config file

---
+++ src/common/settings.ts
@@ -0,0 +1,3 @@
+import * as dotenv from 'dotenv'
+
+export const load = dotenv.config()
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import { boardsRouter } from './controllers/boardsController'
 import { errorMiddleware } from './middlewares/errorMiddleware'
 import { morganMiddleware } from './middlewares/morganMiddleware'
-
+import { load } from './common/settings'
+load
 const app = express()
 app.use(express.json())
 app.use(morganMiddleware)
 app.use('/', boardsRouter)
 app.use(errorMiddleware)
-app.listen(3000, () => {
-  console.log('listen')
+app.listen(process.env.PORT, () => {
+  console.log(`Listen on port ${process.env.PORT}`)
 })
