e149a52577fb7a28cda1d3eb6bae64dbfa0867fd
iKnowiTim <batr.tim@gmail.com>
2023-02-14 21:36:34 +0500

    feat: change settings defaults

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -15,11 +15,11 @@ export interface Settings {
 
 export const settings: Settings = {
   port: parseInt(process.env.PORT ?? '3000'),
-  database: process.env.DB_DATABASE ?? 'todo-backend',
+  database: process.env.DB_DATABASE ?? 'todo',
   dbHost: process.env.DB_HOST ?? 'localhost',
-  dbPort: parseInt(process.env.DB_PORT ?? '5432'),
-  dbName: process.env.DB_USERNAME ?? 'tim',
-  dbPassword: process.env.DB_PASSWORD ?? '76543219q',
+  dbPort: parseInt(process.env.DB_PORT ?? '5433'),
+  dbName: process.env.DB_USERNAME ?? 'todo',
+  dbPassword: process.env.DB_PASSWORD ?? 'todo',
   saltRounds: parseInt(process.env.SALT_ROUNDS ?? '8'),
   secretKey: process.env.SECRET_KEY ?? 'SECRET_KEY_RANDOM',
 }
